2866ad8c87cf2006bf63c01e6e215b70f8444747
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-09-19 15:48:36 -0700

    Use async/await in test

--- adapter/tests/trade.test.js
+++ adapter/tests/trade.test.js
@@ -2,14 +2,14 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('Trade', () => {
-  test('returns the current balance of ETH in the account', () => {
-    return request(app).get('/trades').then(response => {
-      expect(response.statusCode).toBe(200)
-      expect(JSON.parse(response.text)).toEqual({
-        free: 1000,
-        used: 0,
-        total: 1000
-      })
+  test('returns the current balance of ETH in the account', async () => {
+    const response = await request(app).get('/trades')
+
+    expect(response.statusCode).toBe(200)
+    expect(JSON.parse(response.text)).toEqual({
+      free: 1000,
+      used: 0,
+      total: 1000
     })
   })
 })
