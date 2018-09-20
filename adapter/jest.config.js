50c9bb8cb6bce00541ae7724165b3f2412dcc55d
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-09-19 17:10:17 -0700

    Record GDAX API requests with replayer

---
+++ adapter/jest.config.js
@@ -0,0 +1,22 @@
+const replayer = require('replayer')
+
+if (process.env.DEBUG) {
+  replayer.configure({
+    verbose: true,
+    debug: true
+  })
+}
+
+replayer.filter({
+  url: /trades/,
+  urlFilter: url => url.replace(/:[0-9]+/, '')
+})
+
+const sensitive = ['GDAX_API_PASSPHRASE', 'GDAX_API_KEY', 'GDAX_API_SECRET']
+sensitive.forEach(s => {
+  console.log('--- should sub: %o', process.env[s])
+  replayer.substitute(
+    '***',
+    () => process.env[s]
+  )
+})
